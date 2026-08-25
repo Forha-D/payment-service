f052246028e29fdbe180c87c3872de2a4f1273f4
Forhad Kanon <forhadkanon.official@gmail.com>
2026-08-25 10:52:11 +0600

    @EnableScheduling + @EnableConfigurationProperties added and updated

--- src/main/java/com/nexpay/payment/PaymentServiceApplication.java
+++ src/main/java/com/nexpay/payment/PaymentServiceApplication.java
@@ -2,8 +2,14 @@ package com.nexpay.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.nexpay.payment.config.GatewayProperties;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties(GatewayProperties.class)
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
